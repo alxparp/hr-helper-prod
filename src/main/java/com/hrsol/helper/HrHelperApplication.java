305616f41112f460fa462573b1d019d624559e7f
Alex <alex.parpalak@gmail.com>
2023-07-13 15:52:24 +0300

    init

---
+++ src/main/java/com/hrsol/helper/HrHelperApplication.java
@@ -0,0 +1,13 @@
+package com.hrsol.helper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrHelperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hrsol/helper/HrHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrsol.helper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
